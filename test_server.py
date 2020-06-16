7a5d0e0691a0b823cad512a2c80d1be769c45c0a
Yan <yant07@amraelp00007319.pfizer.com>
2020-06-16 03:33:40 -0400

    test server

---
+++ test_server.py
@@ -0,0 +1,16 @@
+#import request
+    
+
+def multi(x):
+    """[summary]
+
+    Args:
+        x ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """        
+    return x * 3
+
+if __name__ =="__main__" :
+    print(multi(2))
